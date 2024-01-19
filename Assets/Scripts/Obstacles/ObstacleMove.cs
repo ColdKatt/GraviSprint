1963e48009bcbd02b6942213cca6ec6f3c91471f
Danil <500459@mail.ru>
2024-01-20 02:28:22 +0500

    Have added destroy position for obstacles

--- Assets/Scripts/Obstacles/ObstacleMove.cs
+++ Assets/Scripts/Obstacles/ObstacleMove.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleMove : MonoBehaviour
 {
+    private float _destroyPosition;
+
+    private void Start()
+    {
+        _destroyPosition = -500.0f;
+    }
+
     private void FixedUpdate()
     {
         transform.position += Vector3.left * GameRoot.EnvironmentSpeed;
+
+        if (transform.position.x < _destroyPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
