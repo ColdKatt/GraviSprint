b0e44e45db490d88dc9aba984a82b82974415ab5
Danil <500459@mail.ru>
2024-01-20 15:08:58 +0500

    Have added Firebase Analytics, Firebase Messaging and OneSignal SDKs

---
+++ Assets/Scripts/OneSignalSync.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OneSignalSDK;
+
+public class OneSignalSync : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OneSignal.Initialize("11245cf0-4b48-4db9-b704-9eea25d7b4e7");
+    }
+}
